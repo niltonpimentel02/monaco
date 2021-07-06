3ca232ce8ae235c4f0fe3f7da38de8181d7b9103
Nilton Pimentel <niltonpimentel02@gmail.com>
2021-07-06 17:20:50 -0300

    updated code with selenium

--- consult_conductor.py
+++ consult_conductor.py
@@ -1,45 +1,83 @@
 import os
-import shutil
-import requests
+from selenium import webdriver
+from time import sleep, time
+from selenium.webdriver.common.keys import Keys
 from pprint import pprint as pp
 from twocaptcha import TwoCaptcha
-from datetime import datetime
 from decouple import config
+from collections import OrderedDict
 
-# Create a link for captcha image based on real
-datetime_now = datetime.now()
-datetime_now_timestamp = datetime.timestamp(datetime_now)
-datetime_now_timestamp_str = str(datetime_now_timestamp)
-datetime_now_timestamp_value = datetime_now_timestamp_str[:14].replace('.', '')
+try:
+    browser = webdriver.Firefox(executable_path='C:\Webdrivers\geckodriver.exe')
 
-# Download captcha image
-url = f'http://consultas.detrannet.sc.gov.br/Servicos/BitMap.asp?{datetime_now_timestamp_value}'
-response = requests.get(url, stream=True)
-with open('img.png', 'wb') as out_file:
-    shutil.copyfileobj(response.raw, out_file)
-del response
+    browser.maximize_window()
 
-# Solve captcha
-solver = TwoCaptcha(config('2_CAPCHA_API_KEY'))
-result = solver.normal('img.png')
-result_code = result['code']
+    sleep(1)
 
-# Delete photo downloaded above
-if os.path.exists("img.png"):
-  os.remove("img.png")
-else:
-  pp("The file does not exist")
+    url = 'http://consultas.detrannet.sc.gov.br/servicos/ConsultaPontuacaoCondutor.asp'
+    browser.get(url=url)
 
-# Post on page
-payload = {'txtDocPrincipal': '04601616956', 'txtDocCNH': '03951668042', 'txtCodigo': f'{result_code}'}
-r = requests.post('http://consultas.detrannet.sc.gov.br/servicos/ConsultaPontuacaoCondutor.asp', data=payload)
+    sleep(1)
 
-# if 'CONSULTA PONTUAÇÃO' not in r.text:
-#     pp('Texto não encontrado')
-# else:
-#     pp('Texto encontrado! Ok!!')
+    # Download captcha image
+    captcha_photo = browser.find_element_by_xpath('//img[@id="imgDesafio"]')
 
-if 'É necessário informar o código.' not in r.text:
-    pp('Texto não encontrado')
-else:
-    pp('Texto encontrado! Ok!!')
+    sleep(2)
+
+    captcha_photo.screenshot('img.png')
+
+    # Solve captcha
+    solver = TwoCaptcha(config('2_CAPCHA_API_KEY'))
+    result = solver.normal('img.png')
+    result_code = result['code']
+
+    # Delete photo downloaded above
+    if os.path.exists('img.png'):
+        os.remove('img.png')
+        print('The file was deleted successfully')
+    else:
+        print('The file does not exist')
+
+    sleep(1)
+
+    cpf = config('CPF')
+    registro = config('REGISTRO')
+    codigo = result_code
+
+    sleep(1)
+
+    cpf_field = browser.find_element_by_name('txtDocPrincipal').send_keys(cpf)
+    registro_field = browser.find_element_by_name('txtDocCNH').send_keys(registro)
+    codigo_field = browser.find_element_by_name('txtCodigo').send_keys(codigo)
+    browser.find_element_by_xpath('//input[@name="btnConsultar"]').click()
+
+    sleep(1)
+
+    data = browser.find_elements_by_xpath('//tr[@width="100%"]')
+
+
+    content = OrderedDict(
+        data_time=data[0].text,
+        data_conductor=data[1].text,
+        data_conductor_name=data[2].text,
+    )
+
+    pp('========================================================================')
+    pp(content)
+    pp('========================================================================')
+    pp(type(content))
+    pp('========================================================================')
+
+    # WIP - Work In Progress
+    data_detail = browser.find_elements_by_xpath('//*[@id="divDadosPontuacao"]/table[2]/tbody/tr')
+    for elements in data_detail:
+        print(elements.text)
+
+    browser.save_screenshot(str(round(time() * 1000)) + ".png")
+
+    browser.quit()
+
+except:
+    print('========================================================================')
+    print('There was a problem with the server! Try again in a few moments, please!')
+    print('========================================================================')
